5f86e8c120df1dce689822d57b9a38f5f2daf75e
nikolasb <nbeltran14@mail.wou.edu>
2017-10-05 10:44:02 -0700

    Added black XCode project.

---
+++ Payday 2 XP Calculator/Payday 2 XP Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Payday 2 XP Calculator
+//
+//  Created by Nikolas Beltran on 10/5/17.
+//  Copyright © 2017 Nikolas Beltran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
